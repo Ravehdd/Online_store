a8f0980d982ae9a3681e13c505818eb36bbfa5ce
max <max.ershov.spb@gmail.com>
2023-12-18 17:14:42 +0300

    отключил пагинацию

--- store/store/settings.py
+++ store/store/settings.py
@@ -168,9 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3,
-
     'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata',
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
